5db75a1f29e591c0af97d43fb5538b865a2f8528
Sean <junglecatsw@gmail.com>
2018-05-17 13:32:54 +0100

    Added type for tag

---
+++ graphql/MenuTagsQuery.js
@@ -0,0 +1,23 @@
+// {
+//     tags(shopId:"cmVhY3Rpb24vc2hvcDpKOEJocTN1VHRkZ3daeDNyeg==", isTopLevel:true) {
+//     pageInfo {
+//         endCursor
+//         startCursor
+//     }
+//     edges {
+//         cursor
+//         node {
+//             position
+//             slug
+//             subTags {
+//                 edges {
+//                     node {
+//                         position
+//                         slug
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+// }
